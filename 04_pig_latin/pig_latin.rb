c43530f406ed2ec548dea8ee012d38c11b141e57
dwdy250 <timdowd1984@gmail.com>
2018-06-11 21:11:31 +0100

    started pig latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,14 @@
-#write your code here
+def translate(s)
+	
+
+	a = s.split("")
+		if a[0] == "a" || a[0] == "e" || a[0] == "i" || a[0] == "o" || a[0] == "u"
+			a.push("ay")
+			s = a.join("")
+			return s
+		else
+			
+		end	
+end
+
+
