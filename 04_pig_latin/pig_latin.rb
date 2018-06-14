376506df9bd55729cc27390fca17234ef77e9b3f
dwdy250 <timdowd1984@gmail.com>
2018-06-14 22:04:10 +0100

    may have to re write from beginning

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,14 +1,57 @@
 def translate(s)
-	
-
+	vowels = ["a","e","i","o","u"]
 	a = s.split("")
-		if a[0] == "a" || a[0] == "e" || a[0] == "i" || a[0] == "o" || a[0] == "u"
-			a.push("ay")
-			s = a.join("")
-			return s
-		else
+
+
+	if a.include?(" ")  #checks if more than 1 word and splits into indivduals words if so and creates nested array
+		a = s.split(" ")
+		a.each do |element|
+			element = element.split("")
+		end
+		puts a
+	end
+	
+	a.each do |element|
+
+		if element.is_a?
+		
+			loop do
+
+			vowels.each do |vowel|
+				if element[0] == vowel
+					element.push("ay") # first check first letter for vowel, and if vowel is present add ay to end of word
+					s = a.join("")
+					
 			
+				end
+			end
+
+			a = a.rotate(1) #if vowel isn't present then its a constonant so send that consonant to the end of the word
+
+		else		
+
+			loop do
+
+				vowels.each do |vowel|
+					if a[0] == vowel
+						a.push("ay") # first check first letter for vowel, and if vowel is present add ay to end of word
+						s = a.join("")
+						return s
+			
+					end
+				end
+
+				a = a.rotate(1) #if vowel isn't present then its a constonant so send that consonant to the end of the word
+
+			end
 		end	
+	end
+
 end
+
+
+
+
+translate("crapple papppa poopooo")
 
 
