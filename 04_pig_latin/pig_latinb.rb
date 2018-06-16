cc4259b82f9cf816f1d562450bf2ca31fd503283
dwdy250 <timdowd1984@gmail.com>
2018-06-16 19:00:41 +0100

    nearly there

--- 04_pig_latin/pig_latinb.rb
+++ 04_pig_latin/pig_latinb.rb
@@ -1,48 +1,53 @@
 def translate(s)
-	vowels = ["a","e","i","o","u"]
+  vowels = ["a","e","i","o","u"]
 
 	if s.split("").include?(" ") # multiple word check
-		a = s.split(" ")
-		a.each do |word|
-			catch (:stop) do
-				loop do
+	  split_sentence = s.split(" ")
+	  split_sentence.each do |word|
+	    catch (:stop) do
+		  loop do
 
-					vowels.each do |vowel|
-						if word[0] == vowel
-							word.push("ay") # first check first letter for vowel, and if vowel is present add ay to end of word
-							puts word
-							throw :stop # exits nested loop to avoid checking the rest of vowels.
+		    vowels.each do |vowel|
+			  if word[0] == vowel
+				word = word.split("")
+				word.push("ay")
+				word = word.join("")
 
-						end
 				
-					end
+				puts word
+				throw :stop # exits nested loop to avoid checking the rest of vowels.
 
-					word = word.rotate(1) #if vowel isn't present then its a constonant so send that consonant to the end of the word
-				end
+			  end
+				
 			end
-		end	
-		
+
+			word = word.rotate(1) #if vowel isn't present then its a constonant so send that consonant to the end of the word
+		  end
+		end
+	  end	
+	
 
 	else
 
-	a = s.split("")
+	  array = s.split("")
 
-				loop do
+	  loop do
 
-					vowels.each do |vowel|
-						if a[0] == vowel
-							a.push("ay") # first check first letter for vowel, and if vowel is present add ay to end of word
-							s = a.join("")
-							puts s
-							return s
+	    vowels.each do |vowel|
+		  if array[0] == vowel
+		    array.push("ay") # first check first letter for vowel, and if vowel is present add ay to end of word
+		    s = array.join("")
+		    puts s
+		    return s
 			
-						end
-					end
+		  end
+	    end
 
-					a = a.rotate(1) #if vowel isn't present then its a constonant so send that consonant to the end of the word
+	    array = array.rotate(1) #if vowel isn't present then its a constonant so send that consonant to the end of the word
 
-				end
+	  end
 	end
+	
 
 end
 
--- 04_pig_latin/pig_latinc.rb
+++ 04_pig_latin/pig_latinc.rb
@@ -2,7 +2,6 @@ def translate(s)
 
   vowels = ["a","e","i","o","u"]
   split_sentence = s.split(" ")
-  pig_sentence = []
 
   split_sentence.each do |word|
     catch (:stop) do
@@ -12,10 +11,10 @@ def translate(s)
 		    word = word.split("") #if so it turns to array and adds ay to end, then turns back to str
 			word.push("ay")
 			word = word.join("")
-			
-			pig_sentence.push(word)
+			puts word
+			return word
 
-  	 	 	throw :stop # exits nested loop to avoid checking the rest of vowels.
+			throw :stop # exits nested loop to avoid checking the rest of vowels.
 
 		  end
 				
@@ -24,16 +23,9 @@ def translate(s)
 		word = word.split("") #if vowel isn't present then its a consonant so send that consonant to the end of the word
 		word = word.rotate(1)
 		word = word.join("")
-		
-
 	  end
 	end
-	
   end
-
-  return pig_sentence.join(" ")
-  
-  
 end
 
 
