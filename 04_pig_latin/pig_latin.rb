f0a3e27faaa82d90d8b7783d9699b79151a3a43a
dwdy250 <timdowd1984@gmail.com>
2018-06-17 20:32:49 +0100

    finished

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -7,8 +7,24 @@ def translate(s)
   split_sentence.each do |word|
     catch (:stop) do
 	  loop do
+
+	  	
+	  	  if word.start_with?("qu") #identifies qu phoneme (block that deals with consonants also deals with sch phoneme)
+	  	  	word = word.split("")
+	  	  	word = word.rotate(2)
+	  	  	word.push("ay")
+	  	  	word = word.join("")
+
+	  	  	pig_sentence.push(word)
+
+		    throw :stop # exits nested loop to avoid checking the rest of vowels.
+			
+			
+		  end
+	    
+
         vowels.each do |vowel| #checks if first letter of word is a vowel
-		  if word[0] == vowel
+		  if word.start_with?(vowel)
 		    word = word.split("") #if so it turns to array and adds ay to end, then turns back to str
 			word.push("ay")
 			word = word.join("")
@@ -31,10 +47,15 @@ def translate(s)
 	
   end
 
+  puts pig_sentence.join(" ")
   return pig_sentence.join(" ")
   
   
+  
+  
 end
+
+
 
 
 
