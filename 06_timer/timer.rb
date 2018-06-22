b34c114bde70987ce05a0a469d9d4476c3388083
dwdy250 <timdowd1984@gmail.com>
2018-06-22 11:11:57 +0100

    finished all

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,54 @@
 class Timer
-  #write your code here
+  def initialize
+  	@seconds = 0
+  end 
+
+
+  def seconds=(seconds) #setter
+    
+  	@seconds = seconds
+  	
+  	hours = 0
+  	minutes = 0
+
+
+  	while seconds >= 3600
+  	  hours += 1
+  	  seconds -= 3600
+  	end
+
+  	while seconds >= 60
+  		minutes += 1
+  		seconds -= 60
+  	end
+
+  	if hours < 9
+  	  hours = "0" + hours.to_s
+    end
+
+    if minutes < 9
+      minutes = "0" + minutes.to_s
+    end
+
+    if seconds < 9
+      seconds = "0" +seconds.to_s
+    end
+
+
+  	time_format = [hours,minutes,seconds]
+  	@time_string = time_format.join(":")
+
+  end
+
+
+  def time_string #getter
+  	@time_string
+  end	
+
+
+  def seconds #getter
+  	@seconds
+  end
+
+  
 end
